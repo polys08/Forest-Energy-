5090f1766d8aa2836663a8d990bf06c7ed70fea8
polys08 <polyana.lima@grupointegrado.br>
2026-04-08 10:24:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ hamburger.addEventListener('click', () => {
     // Alterna a classe 'active' no hamburger para animar as linhas
     hamburger.classList.toggle('active');
 });
+
+// pega os elementos
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+// pega todos os links do menu
+const links = document.querySelectorAll('.nav-list a');
+
+// quando clicar em um link
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');        // fecha o menu
+        hamburger.classList.remove('active');  // animação do botão volta
+    });
+});
