788ed2c01059ca9f993993d9725899b9d596a5ed
polys08 <polyana.lima@grupointegrado.br>
2026-04-16 20:41:25 -0300

    Refactor hamburger menu toggle functionality

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-// Pega os elementos do DOM
-const hamburger = document.getElementById('hamburger'); // botão hamburger
-const nav = document.getElementById('nav'); // menu de navegação
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
 
-// Quando o usuário clicar no hamburger
+
 hamburger.addEventListener('click', () => {
-    // Alterna a classe 'open' no menu para mostrar/esconder
+    
     nav.classList.toggle('active');
 
-    // Alterna a classe 'active' no hamburger para animar as linhas
+
     hamburger.classList.toggle('active');
 });
 
-// pega todos os links do menu
+
 const links = document.querySelectorAll('.nav-list a');
 
-// quando clicar em um link
+
 links.forEach(link => {
     link.addEventListener('click', () => {
-        nav.classList.remove('active');        // fecha o menu
-        hamburger.classList.remove('active');  // animação do botão volta
+        nav.classList.remove('active');
+        hamburger.classList.remove('active');
     });
 });
